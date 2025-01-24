26d6c4660aa76ed119828f5deecfd0014043f683
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-01-24 14:11:28 -0600

    Time: 826 ms (8.92%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,10 +1,20 @@
 # Write your MySQL query statement below
-with acp as (select accepter_id id,count(requester_id ) num from RequestAccepted 
-group by accepter_id),
-req as (select requester_id id,count(accepter_id ) num from RequestAccepted 
-group by requester_id)
-select id, sum(num) num  from (
-    select * from acp
-union all 
-select * from req) a
-group  by id order by num desc limit 1
+-- with acp as (select accepter_id id,count(requester_id ) num from RequestAccepted 
+-- group by accepter_id),
+-- req as (select requester_id id,count(accepter_id ) num from RequestAccepted 
+-- group by requester_id)
+-- select id, sum(num) num  from (
+--     select * from acp
+-- union all 
+-- select * from req) a
+-- group  by id order by num desc limit 1
+
+
+select id , count(id) as num
+from(
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted) as total
+group by id
+order by count(id) desc
+limit 1;
