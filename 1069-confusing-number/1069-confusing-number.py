d276c438f2b33b62f0764cda2b580f9e0d984c1f
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-01-25 20:50:15 -0600

    Time: 0 ms (100%), Space: 18 MB (7.92%) - LeetHub

---
+++ 1069-confusing-number/1069-confusing-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def confusingNumber(self, n: int) -> bool:
+        inverse={1:1,0:0,6:9 ,9:6,8:8}
+        og=n
+        res=0
+        f=1
+        stack=[]
+        while n:
+            c=n%10
+            n=n//10
+            if c not in inverse:
+                return False
+            else:
+                stack.append(inverse[c])
+        while stack:
+            c=stack.pop()
+            res+=f*c
+            f*=10
+        print(og,res)
+        return True if  res!=og else False
