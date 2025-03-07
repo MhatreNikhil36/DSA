43975e25fd37f4b8a0abf7b28fc471640a785a4b
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-03-06 21:08:24 -0600

    Time: 22 ms (8.55%), Space: 23.3 MB (61.34%) - LeetHub

---
+++ 1200-remove-interval/1200-remove-interval.py
@@ -0,0 +1,23 @@
+class Solution:
+    def removeInterval(self, intervals: List[List[int]], toBeRemoved: List[int]) -> List[List[int]]:
+        s=toBeRemoved[0]
+        e=toBeRemoved[1]
+        ans=[]
+        for x,y in intervals:
+            if x==s and y==e:
+                continue
+            if x>e:
+                ans.append([x,y])
+            elif y<s:
+                ans.append([x,y])
+            elif x>=s and x<e and y>e:
+                ans.append([e,y])
+            elif x<=s and y>=s and y<=e:
+                ans.append([x,s])
+            elif x<s and y>e:
+                ans.append([x,s])
+                ans.append([e,y])
+        print(ans)
+        return ans
+
+
