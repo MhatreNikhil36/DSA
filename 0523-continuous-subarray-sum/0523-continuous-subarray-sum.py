0f1fbae311cedb0ae2e668af3158d31d32039d47
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-04-14 12:22:08 -0500

    Time: 55 ms (38.44%), Space: 38.1 MB (72.5%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,17 +1,14 @@
 class Solution:
-    def checkSubarraySum(self, nums, k):
-        prefix_mod = 0
-        mod_seen = {0: -1}
-
-        for i in range(len(nums)):
-            prefix_mod = (prefix_mod + nums[i]) % k
-
-            if prefix_mod in mod_seen:
-                # ensures that the size of subarray is at least 2
-                if i - mod_seen[prefix_mod] > 1:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        rems = {0:-1}
+        total = 0
+        for i, n in enumerate(nums):
+            total += n
+            rem = total % k
+            if rem in rems:
+                if i - rems[rem] >= 2:
                     return True
             else:
-                # mark the value of prefix_mod with the current index.
-                mod_seen[prefix_mod] = i
-
+                rems[rem] = i
         return False
+
