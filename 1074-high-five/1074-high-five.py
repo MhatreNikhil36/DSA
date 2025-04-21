b541c43d23d497690b4168bf655a467ca716f908
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-04-21 16:39:11 -0500

    Time: 6 ms (4.53%), Space: 18 MB (52.75%) - LeetHub

--- 1074-high-five/1074-high-five.py
+++ 1074-high-five/1074-high-five.py
@@ -1,39 +1,27 @@
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
         students={}
-        for item in items:
-            
-            if item[0] in students:
-                heappush(students[item[0]], item[1])
-                if len(students[item[0]])>5:
-                    x=heappop(students[item[0]])
-                    # print('added',item[1],'removed',x)
-            else:
-                students[item[0]]=[item[1]]
-        # # 
-        # for x in students:
-        #     # print(x,students[x])
-        ans=[]
-        def topKavg(heap,k):
-            sum=0
-            c=0
-            heapify(heap)
-            for i in range(k):
+        id=[]
+        res=[]
+        for x in items:
+            xId,xScore=x
+            if xId not in students:
+                students[xId]=[]
+                heapq.heapify(students[xId])
+                heapq.heappush(id,xId)
+            print(xId,students[xId])
+            if len(students[xId])>=5:
+                if  xScore>students[xId][0]:
+                    heapq.heappop(students[xId])
+                    heapq.heappush(students[xId],xScore)
 
-                if not heap:
-                    break
-                c+=1
-                kth=-heappop(heap)
-                # print(c,kth)
-                sum+=kth
-            if c:
-                return sum//c
-            return 0
-        for student in students:
-            # print('curr student',student)
-            # print(students[student])
-            x=sum(students[student])//len(students[student])
-            # x=topKavg(students[student],5)
-            ans.append([student,x])
-        return sorted(ans,key=lambda x:x[0])
+            else:
+                heapq.heappush(students[xId],xScore)
+        while id :
+            i=heapq.heappop(id)
+            res.append([i,sum(students[i])//5])
+        return res
             
+
+
+
