aff415f23393e30b0d9ca24645c261a2049b031a
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-04-29 16:40:28 -0500

    Time: 11 ms (9.09%), Space: 18.3 MB (7.27%) - LeetHub

---
+++ 0604-design-compressed-string-iterator/0604-design-compressed-string-iterator.py
@@ -0,0 +1,41 @@
+class StringIterator:
+
+    def __init__(self, compressedString: str):
+        self.comp=deque(list(compressedString))
+        self.currs=None
+        self.currc=0
+        
+
+    def next(self) -> str:
+        if not self.comp and self.currc==0:
+            return " "
+        if self.currc==0:
+            self.currs=self.comp.popleft()
+            p=0
+            while self.comp and self.isdigit(self.comp[0]):
+                x=self.comp.popleft()
+                self.currc=(self.currc*(10**p))+int(x)
+                p+=1
+        print(self.currc)
+        self.currc-=1
+        return self.currs
+    
+    def isdigit(self,x):
+        if x in {'0','1','2','3','4','5','6','7','8','9'}:
+            return True
+        return False
+
+
+        
+
+    def hasNext(self) -> bool:
+        if self.comp or self.currc>0:
+            return True
+        return False
+        
+
+
+# Your StringIterator object will be instantiated and called as such:
+# obj = StringIterator(compressedString)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
