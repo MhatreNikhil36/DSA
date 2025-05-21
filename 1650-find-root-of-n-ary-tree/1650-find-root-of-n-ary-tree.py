b3015f9ad1c80224677a04fc4c33921892aafb98
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-05-21 01:36:16 -0500

    Time: 101 ms (41.5%), Space: 25.9 MB (76.8%) - LeetHub

--- 1650-find-root-of-n-ary-tree/1650-find-root-of-n-ary-tree.py
+++ 1650-find-root-of-n-ary-tree/1650-find-root-of-n-ary-tree.py
@@ -8,17 +8,28 @@ class Node:
 
 class Solution:
     def findRoot(self, tree: List['Node']) -> 'Node':
-        ind=dict()
-        for x in tree:
-            if x not in ind:
-                ind[x]=0
-            for y in x.children:
-                if y not in ind:
-                    ind[y]=0
-                ind[y]+=1
-        # print(ind)
+        # ind=dict()
+        # for x in tree:
+        #     if x not in ind:
+        #         ind[x]=0
+        #     for y in x.children:
+        #         if y not in ind:
+        #             ind[y]=0
+        #         ind[y]+=1
+        # # print(ind)
 
-        for x in ind:
-            if ind[x]==0:
-                return x 
-        return -1
+        # for x in ind:
+        #     if ind[x]==0:
+        #         return x 
+        # return -1
+        # above is a logical sane solution below is dibpolicaly genious solution that i have no idea how any one in thjeor right mind can come up with 
+
+        ans=0
+        for x in tree:
+            ans+=x.val
+            for y in x.children:
+                ans-=y.val
+        for x in tree:
+            if x.val==ans:
+                return x
+        return None 
