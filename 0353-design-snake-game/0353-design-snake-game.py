74aa34c4050031a25381eea3e827a2f0aff259d9
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-06-08 18:49:53 -0500

    Time: 335 ms (5.01%), Space: 19.6 MB (25.91%) - LeetHub

---
+++ 0353-design-snake-game/0353-design-snake-game.py
@@ -0,0 +1,45 @@
+class SnakeGame:
+
+    def __init__(self, width: int, height: int, food: List[List[int]]):
+        self.pos=(0,0)
+        self.size=1
+        self.moves={'R':(0,1),'L':(0,-1),'U':(-1,0),'D':(1,0)}
+        self.food=food
+        self.bounds=(height,width)
+        print('bounds',self.bounds)
+        self.snake=deque()
+        self.snake.append((0,0))
+    def move(self, direction: str) -> int:
+        x,y=self.pos
+        dx,dy=self.moves[direction]
+        
+        nx=x+dx
+        ny=y+dy
+        l=self.snake.popleft()
+        # print(nx,ny)
+        print(self.food,direction)
+        print('change',direction,'nx,ny',nx,ny)
+        print('snake',self.snake,'\n','---'*10)
+        if 0<=nx<self.bounds[0] and 0<=ny<self.bounds[1] and (nx,ny) not in self.snake:
+            self.pos=(nx,ny)
+            if self.food and nx==self.food[0][0] and  ny==self.food[0][1]:
+                if len(self.food)>1:
+                    self.food=self.food[1:]
+                else:
+                    self.food=[]
+                self.size+=1
+                self.snake.appendleft(l)
+                self.snake.append(self.pos)
+            else:
+                
+                self.snake.append(self.pos)
+            return self.size-1
+
+        else:
+            return -1
+
+
+
+# Your SnakeGame object will be instantiated and called as such:
+# obj = SnakeGame(width, height, food)
+# param_1 = obj.move(direction)
