632a2e8fb00b33d7f3b2a8b91c54f9833981e34b
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-02 13:41:17 -0500

    Time: 1223 ms (18.18%), Space: 0B (100%) - LeetHub

--- 1565-evaluate-boolean-expression/1565-evaluate-boolean-expression.sql
+++ 1565-evaluate-boolean-expression/1565-evaluate-boolean-expression.sql
@@ -1,12 +1,9 @@
 # Write your MySQL query statement below
 select e.left_operand , e.operator,e.right_operand, case 
 when e.operator='=' and ifnull(va.value,0)=ifnull(vb.value,0) then 'true' 
-when e.operator='=' and ifnull(va.value,0)<>ifnull(vb.value,0) then 'false' 
-when e.operator='<' and ifnull(va.value,0)>=ifnull(vb.value,0) then 'false' 
 when e.operator='<' and ifnull(va.value,0)<ifnull(vb.value,0) then 'true'  
-when e.operator='>' and ifnull(va.value,0)<=ifnull(vb.value,0) then 'false'
 when e.operator='>' and ifnull(va.value,0)>ifnull(vb.value,0) then 'true' 
-Else null
+Else 'false'
 end as value from
 Expressions e
 left join Variables va  on va.name=e.left_operand  
