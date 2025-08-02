b800275d812c9cf533fa13bec569775ddca3b061
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-02 11:50:55 -0500

    Time: 1547 ms (26.76%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,9 +1,11 @@
-SELECT s.name
-FROM SalesPerson s
-LEFT JOIN (
-    SELECT DISTINCT o.sales_id
-    FROM Orders o
-    JOIN Company c ON o.com_id = c.com_id
-    WHERE c.name = 'RED'
-) red_sales ON s.sales_id = red_sales.sales_id
-WHERE red_sales.sales_id IS NULL;
+
+select s.name from SalesPerson s
+left join (
+select o.sales_id from orders o 
+inner join Company c
+on c.com_id  =o.com_id
+where c.name='RED'
+
+) redSales
+on s.sales_id=redSales.sales_id
+where redSales.sales_id is Null
