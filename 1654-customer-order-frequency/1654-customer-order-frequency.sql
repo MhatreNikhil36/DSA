b96580f4f5d9363d3cd8162b291cd17e0cbad0e9
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-03 18:45:51 -0500

    Time: 783 ms (97.28%), Space: 0B (100%) - LeetHub

--- 1654-customer-order-frequency/1654-customer-order-frequency.sql
+++ 1654-customer-order-frequency/1654-customer-order-frequency.sql
@@ -1,24 +1,18 @@
-select c.customer_id,c.name from  customers c
-inner join(
-select a.customer_id
-from (
-select o.customer_id,o.product_id,o.quantity*p.price as spent1   from  orders  o
-left join  Product p
-on p.product_id=o.product_id
-WHERE EXTRACT(YEAR FROM order_date) = 2020
-  AND EXTRACT(MONTH FROM order_date) IN (6)) a
-inner  join 
- (
-select o.customer_id,o.product_id,o.quantity*p.price as spent2     from  orders  o
-left join  Product p
-on p.product_id=o.product_id
-WHERE EXTRACT(YEAR FROM order_date) = 2020
-  AND EXTRACT(MONTH FROM order_date) IN (7)
-  
-  ) b on
-  a.customer_id=b.customer_id
-group by  a.customer_id
-having sum(spent1)>=100 and sum(spent2)>=100
-) b
-on c.customer_id=b.customer_id
-
+SELECT 
+  customer_id, 
+  name 
+FROM 
+  Customers 
+  JOIN Orders USING(customer_id) 
+  JOIN Product USING(product_id) 
+WHERE 
+  YEAR(order_date)= 2020 
+GROUP BY 
+  customer_id 
+HAVING 
+  SUM(
+      IF(MONTH(order_date) = 6, quantity, 0) * price
+  ) >= 100 AND 
+  SUM(
+      IF(MONTH(order_date) = 7, quantity, 0) * price
+  ) >= 100;
