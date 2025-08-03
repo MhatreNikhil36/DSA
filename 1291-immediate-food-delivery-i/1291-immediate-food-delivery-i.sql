3e2a9ed7f96954e742444c6e98d38fd905a644da
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-03 13:14:55 -0500

    Time: 434 ms (56.06%), Space: 0B (100%) - LeetHub

--- 1291-immediate-food-delivery-i/1291-immediate-food-delivery-i.sql
+++ 1291-immediate-food-delivery-i/1291-immediate-food-delivery-i.sql
@@ -1,2 +1,5 @@
-select round(avg(order_date=customer_pref_delivery_date)*100 ,2) immediate_percentage 
+-- select round(avg(order_date=customer_pref_delivery_date)*100 ,2) immediate_percentage 
+-- from Delivery
+
+select round(100*count(case when order_date=customer_pref_delivery_date then 1 end)/count(1) ,2) immediate_percentage 
 from Delivery
