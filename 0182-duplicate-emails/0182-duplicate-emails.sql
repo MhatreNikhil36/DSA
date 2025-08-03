cd6ad93f4ac2ccd77917babf1746f5759c4b185b
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-03 16:23:27 -0500

    Time: 368 ms (58.04%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
-select email from(
-select email, row_number() over(partition by email) as  rn
-from person 
-where email is not null
-) as emails
-where rn=2
+select email   
+from person
+group by email
+having count(email)>1
