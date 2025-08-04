ab0aaec2d5887d4f0ce00dc4af0ddd81e8afb368
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-04 00:03:32 -0500

    Time: 1315 ms (87.07%), Space: 0B (100%) - LeetHub

---
+++ 1932-grand-slam-titles/1932-grand-slam-titles.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+select p.player_id ,p.player_name,count(id)  grand_slams_count 
+from
+players p left join
+(
+select Wimbledon    id
+from Championships
+union all 
+select Fr_open           id
+from Championships
+union all 
+select us_open           id
+from Championships
+union all 
+select au_open           id
+from Championships
+) a
+on p.player_id=a.id
+group by p.player_name
+having grand_slams_count>0
