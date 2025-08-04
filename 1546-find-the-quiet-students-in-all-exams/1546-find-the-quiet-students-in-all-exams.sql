64e85e7a62c2228c70ed23d836b12ae913bd7851
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-04 16:09:48 -0500

    Time: 684 ms (10.06%), Space: 0B (100%) - LeetHub

--- 1546-find-the-quiet-students-in-all-exams/1546-find-the-quiet-students-in-all-exams.sql
+++ 1546-find-the-quiet-students-in-all-exams/1546-find-the-quiet-students-in-all-exams.sql
@@ -1,15 +1,19 @@
-
-select s.student_id,s.student_name from student s
-join 
-(
-
-select q.student_id , sum(case when q.score  between e.low+1 and e.high-1 then 0 else 1  end) as notquite
-from exam q
-left join 
-(select exam_id, min(score) low, max(score) high 
-from exam group by exam_id) e 
-on q.exam_id =e.exam_id
-group by q.student_id
-) e
-on s.student_id=e.student_id 
-where notquite=0
+SELECT s.student_id, s.student_name
+FROM student s
+JOIN (
+    SELECT e.student_id
+    FROM exam e
+    JOIN (
+        SELECT exam_id, MIN(score) AS low, MAX(score) AS high
+        FROM exam
+        GROUP BY exam_id
+    ) r ON e.exam_id = r.exam_id
+    GROUP BY e.student_id
+    HAVING SUM(
+        CASE 
+            WHEN e.score = r.low OR e.score = r.high THEN 1 
+            ELSE 0 
+        END
+    ) = 0
+) quiet_students ON s.student_id = quiet_students.student_id
+ORDER BY s.student_id;
