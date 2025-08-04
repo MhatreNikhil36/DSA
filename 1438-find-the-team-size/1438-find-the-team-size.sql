ba8a50a53f91b8581753390ab53e13e18099dac4
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-04 10:51:06 -0500

    Time: 308 ms (21.04%), Space: 0B (100%) - LeetHub

--- 1438-find-the-team-size/1438-find-the-team-size.sql
+++ 1438-find-the-team-size/1438-find-the-team-size.sql
@@ -1,6 +1,4 @@
-select e.employee_id , ifnull(t.team_size,0) team_size from  employee e
-left join 
-(
-select team_id,count(1) team_size
-from employee
-group by team_id) t on e.team_id=t.team_id
+SELECT 
+  employee_id,
+  COUNT(*) OVER (PARTITION BY team_id) AS team_size
+FROM Employee;
