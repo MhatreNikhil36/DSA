9542753eb866d406874b9f936dc1ef18c7a77a94
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-03 19:10:01 -0500

    Time: 888 ms (64.96%), Space: 0B (100%) - LeetHub

---
+++ 1639-friendly-movies-streamed-last-month/1639-friendly-movies-streamed-last-month.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select distinct title from  content c
+ join
+(
+select content_id,program_date  from   
+tvprogram  
+where extract(year from program_date)=2020  and extract(month from program_date)=6  
+) t
+on t.content_id=c.content_id
+where c.Kids_content='Y'  and content_type ='Movies' 
+-- and t.content_id is not null
