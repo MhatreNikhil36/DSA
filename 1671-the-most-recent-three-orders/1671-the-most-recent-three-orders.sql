73f126d74da13a9c2ca0b000f57de805f052eb64
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-04 13:49:52 -0500

    Time: 1564 ms (5.69%), Space: 0B (100%) - LeetHub

--- 1671-the-most-recent-three-orders/1671-the-most-recent-three-orders.sql
+++ 1671-the-most-recent-three-orders/1671-the-most-recent-three-orders.sql
@@ -1,9 +1,9 @@
 select c.name as customer_name, c.customer_id, o.order_id,o.order_date
 from customers c
-left join 
+ join 
 (select customer_id,order_id,order_date, row_number() over(partition by customer_id order by order_date desc) rn
 from orders
 ) as o
 on  o.customer_id=c.customer_id
-where o.rn<4 and o.customer_id is not null
+where o.rn<4 
 order by c.name, c.customer_id,o.order_date desc
