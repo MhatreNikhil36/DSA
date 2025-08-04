fb1ff9b4c937ceacb4667e56711b2debd783ed07
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-04 13:59:54 -0500

    Time: 600 ms (24.13%), Space: 0B (100%) - LeetHub

--- 1981-maximum-transaction-each-day/1981-maximum-transaction-each-day.sql
+++ 1981-maximum-transaction-each-day/1981-maximum-transaction-each-day.sql
@@ -1,9 +1,9 @@
-select t.transaction_id from
-(
-select day,max(amount) mamount
-from transactions
-group by  day ) d
-join 
-transactions t on t.day=d.day
-where t.amount=d.mamount
-order by transaction_id 
+SELECT transaction_id
+FROM (
+    SELECT 
+        transaction_id,
+        rank() OVER (PARTITION BY day ORDER BY amount DESC) AS rn
+    FROM transactions
+) r
+WHERE rn = 1
+ORDER BY transaction_id;
