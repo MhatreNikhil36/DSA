3972fa130961d0a90609e40a5de1820abc39c436
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-04 15:23:34 -0500

    Time: 241 ms (58.26%), Space: 0B (100%) - LeetHub

--- 1405-all-people-report-to-the-given-manager/1405-all-people-report-to-the-given-manager.sql
+++ 1405-all-people-report-to-the-given-manager/1405-all-people-report-to-the-given-manager.sql
@@ -1,9 +1,12 @@
-SELECT DISTINCT e1.employee_id
-FROM Employees e1
-LEFT JOIN Employees e2 ON e1.manager_id = e2.employee_id
-LEFT JOIN Employees e3 ON e2.manager_id = e3.employee_id
-LEFT JOIN Employees e4 ON e3.manager_id = e4.employee_id
-WHERE 
-    (e1.manager_id = 1 and e1.employee_id!=1) OR
-    (e2.manager_id = 1 and e2.employee_id!=1)OR
-    (e3.manager_id = 1 and e3.employee_id!=1)
+# Write your MySQL query statement below
+
+with direct as (
+select employee_id from employees where manager_id=1 and employee_id!=1)
+, lvl2 as (
+select employee_id from employees where manager_id in (select employee_id from direct))
+
+select employee_id from employees where manager_id in (select employee_id from lvl2)
+union all
+select employee_id from direct
+union all
+select employee_id from lvl2
