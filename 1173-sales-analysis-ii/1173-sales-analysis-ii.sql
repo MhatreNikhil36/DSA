053468ec841b1cbf899163e490a5bb681f7f4a39
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-06 23:50:52 -0500

    Time: 1134 ms (25.25%), Space: 0B (100%) - LeetHub

--- 1173-sales-analysis-ii/1173-sales-analysis-ii.sql
+++ 1173-sales-analysis-ii/1173-sales-analysis-ii.sql
@@ -1,9 +1,7 @@
-# Write your MySQL query statement below
-select u.buyer_id from sales u
-join (
-select product_id , case when product_name='S8' then 1 else 0 end as is_s8
-from product  where product_name='S8' or product_name='iPhone')p 
-on p.product_id=u.product_id
-group by u.buyer_id
-having count(1)=sum(is_s8)
-
+SELECT buyer_id
+FROM Sales
+JOIN product
+USING(product_id)
+GROUP BY buyer_id
+HAVING SUM(CASE WHEN product_name = 'S8' THEN 1 ELSE 0 END) > 0
+AND SUM(CASE WHEN product_name = 'iPhone' THEN 1 ELSE 0 END) = 0;
