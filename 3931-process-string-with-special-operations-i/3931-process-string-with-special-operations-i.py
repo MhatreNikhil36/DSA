8891beeb4512d296d19f25558759f97443a7c71d
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-07 18:37:14 -0500

    Time: 51 ms (11.46%), Space: 27.2 MB (23.43%) - LeetHub

---
+++ 3931-process-string-with-special-operations-i/3931-process-string-with-special-operations-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        res=[]
+
+        operations={'*','#','%'}
+        for  x in s:
+            print(x)
+            if  x not in operations:
+                res.append(x)
+            elif  x=='*':
+                if res:
+                    res.pop()
+            elif x=='#':
+                res=res+res
+                
+            elif x=='%':
+                res=res[::-1]
+            # print('\t',res)
+        return ''.join(res)
