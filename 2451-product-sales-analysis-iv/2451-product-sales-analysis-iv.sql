5f54c000c50aa0aba4e54cbbf82c4c8e02ab7b97
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-07 11:42:28 -0500

    Time: 748 ms (78.5%), Space: 0B (100%) - LeetHub

---
+++ 2451-product-sales-analysis-iv/2451-product-sales-analysis-iv.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select user_id,product_id from (
+select s.user_id,s.product_id, sum(p.price * s.quantity) s_moneySpent,
+rank() over( partition by s.user_id order by sum(p.price * s.quantity) desc) r
+from  sales s
+left join 
+product p on p.product_id =s.product_id  
+group by s.user_id,s.product_id
+) a where r=1
