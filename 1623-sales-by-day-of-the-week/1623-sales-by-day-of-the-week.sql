582eb5f6cb018816f8091e4813a5de1c53154d45
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-09 10:54:36 -0500

    Time: 516 ms (52.85%), Space: 0B (100%) - LeetHub

--- 1623-sales-by-day-of-the-week/1623-sales-by-day-of-the-week.sql
+++ 1623-sales-by-day-of-the-week/1623-sales-by-day-of-the-week.sql
@@ -1,26 +1,14 @@
-with  pivot  as (
 SELECT 
-    i.item_category CATEGORY,
-    case when WEEKDAY(order_date)=0 then SUM(quantity)  else 0 end  as MONDAY ,
-    case when WEEKDAY(order_date)=1 then SUM(quantity)  else 0 end  as TUESDAY, 
-    case when WEEKDAY(order_date)=2 then SUM(quantity)  else 0 end  as WEDNESDAY, 
-    case when WEEKDAY(order_date)=3 then SUM(quantity)  else 0 end  as THURSDAY  ,
-    case when WEEKDAY(order_date)=4 then SUM(quantity)  else 0 end  as FRIDAY  ,
-    case when WEEKDAY(order_date)=5 then SUM(quantity)  else 0 end  as SATURDAY ,  
-    case when WEEKDAY(order_date)=6 then SUM(quantity)  else 0 end  as SUNDAY  
-from Items i
-LEFT JOIN  Orders  os
+    i.item_category AS category,
+    SUM(CASE WHEN WEEKDAY(os.order_date) = 0 THEN os.quantity ELSE 0 END) AS monday,
+    SUM(CASE WHEN WEEKDAY(os.order_date) = 1 THEN os.quantity ELSE 0 END) AS tuesday,
+    SUM(CASE WHEN WEEKDAY(os.order_date) = 2 THEN os.quantity ELSE 0 END) AS wednesday,
+    SUM(CASE WHEN WEEKDAY(os.order_date) = 3 THEN os.quantity ELSE 0 END) AS thursday,
+    SUM(CASE WHEN WEEKDAY(os.order_date) = 4 THEN os.quantity ELSE 0 END) AS friday,
+    SUM(CASE WHEN WEEKDAY(os.order_date) = 5 THEN os.quantity ELSE 0 END) AS saturday,
+    SUM(CASE WHEN WEEKDAY(os.order_date) = 6 THEN os.quantity ELSE 0 END) AS sunday
+FROM Items i
+LEFT JOIN Orders os
     ON os.item_id = i.item_id
-group by category,WEEKDAY(os.order_date)) 
-select 
-    CATEGORY, 
-    sum(MONDAY) MONDAY,
-    sum(TUESDAY ) TUESDAY ,
-    sum(WEDNESDAY) WEDNESDAY,
-    sum(THURSDAY ) THURSDAY ,
-    sum(FRIDAY ) FRIDAY ,
-    sum(SATURDAY ) SATURDAY ,
-    sum(SUNDAY ) SUNDAY 
-from pivot
-group by category
-order by category
+GROUP BY i.item_category
+ORDER BY i.item_category;
