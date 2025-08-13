5bd5c5095c390c62d455e898c0e06d5694bd716d
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-08-13 11:15:26 -0500

    Time: 158 ms (35.55%), Space: 78 MB (57.37%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,31 +1,56 @@
-class LRUCache:
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
 
+
+class LRUCache:
     def __init__(self, capacity: int):
-        self.lru = {}
-        self.stack = []
-        self.cap = capacity
+        self.capacity = capacity
+        self.dic = {}
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
-        if key in self.lru:
-            # Move the accessed key to the top of the stack
-            self.stack.remove(key)
-            self.stack.append(key)
-            return self.lru[key][0]
-        else:
+        if key not in self.dic:
             return -1
 
+        node = self.dic[key]
+        self.remove(node)
+        self.add(node)
+        return node.val
+
     def put(self, key: int, value: int) -> None:
-        if key in self.lru:
-            # Update existing key's value and move it to the top of the stack
-            self.lru[key] = (value, len(self.stack))
-            self.stack.remove(key)
-            self.stack.append(key)
-        else:
-            # Add new key-value pair
-            if len(self.stack) >= self.cap:
-                # Remove the least recently used key from the stack and dictionary
-                lru_key = self.stack.pop(0)
-                del self.lru[lru_key]
-            # Add the new key to the top of the stack and dictionary
-            self.lru[key] = (value, len(self.stack))
-            self.stack.append(key)
+        if key in self.dic:
+            old_node = self.dic[key]
+            self.remove(old_node)
+
+        node = ListNode(key, value)
+        self.dic[key] = node
+        self.add(node)
+
+        if len(self.dic) > self.capacity:
+            node_to_delete = self.head.next
+            self.remove(node_to_delete)
+            del self.dic[node_to_delete.key]
+
+    def add(self, node):
+        previous_end = self.tail.prev
+        previous_end.next = node
+        node.prev = previous_end
+        node.next = self.tail
+        self.tail.prev = node
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
